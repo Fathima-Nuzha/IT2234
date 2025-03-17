066f3426083c32cd68d36d56fecac4ea2a97fecb
Fathima-Nuzha <nuzhaimthikaf123@gmail.com>
2025-03-17 21:16:20 +0530

    Add files via upload

---
+++ CODES/StringArrayBasics.js
@@ -0,0 +1,14 @@
+let B=["APPLE","ORANGE","MANGO"]
+
+for(let i=0;i<B.length;i++){
+	console.log(B[i]);
+}
+console.log("")
+
+
+let C=['A','B','N']
+
+for(let i=0;i<C.length;i++){
+	console.log(C[i]);
+}
+console.log("")
